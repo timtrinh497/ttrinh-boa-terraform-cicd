25fe1a53ad7663479647be741ed77cf3c086ef76
timtrinh497 <timtrinh497@comcast.net>
2025-11-19 15:46:10 +0000

    my terraform codes

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "example" {
-  ami           = var.my-os-image
+  ami           = "${var.my-os-image}"
   instance_type = var.machine_size
-  key_name      = var.private_key_name
-
+  key_name = var.private_key_name
+  # adding security group to ec2 vm
+  vpc_security_group_ids = [aws_security_group.ttrinh_sec-groups.id]
+ 
   tags = {
     Name = var.vm_name
   }
--- security-groups.tf
+++ security-groups.tf
@@ -3,21 +3,22 @@ resource "aws_security_group" "ttrinh_sec-groups" {
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = data.aws_vpc.ttrinh-exiting-vpc.id
   
-  ingress {
-    description = "Allow HTTP from anywhere"
-    from_port   = 20
-    to_port     = 20
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "Allow HTTP from anywhere"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  ingress {
-    description = "Allow HTTPS from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "Allow HTTPS from anywhere"
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+
   tags = {
     Name = "ttrinh_firewall_rules"
   }
@@ -27,4 +28,22 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.ttrinh_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+# allow 22 port from everywhere
+resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.ttrinh_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol       = "tcp"
+  to_port = 22
+}
+
+# allow 80 port from everywhere
+resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv4_http" {
+  security_group_id = aws_security_group.ttrinh_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port = 80
+  ip_protocol       = "tcp"
+  to_port = 80
 }
